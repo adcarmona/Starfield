023b3ccd943938dc807e59fdf42ce3098031508e
adcarmona <ancarmona@697-334-Waymoot.sf.k12.internal>
2014-10-14 11:23:44 -0700

    First Commit

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle [] Particles;
 boolean warp = false;
 boolean mouseControl = false;
-void setup()
+public void setup()
 {
 	size(500,500);
 	background(0);
@@ -13,7 +29,7 @@ void setup()
  		Particles[i] = new NormalParticle();
  	}
 }
-void draw()
+public void draw()
 {
 	if (key == 'm')
 	{
@@ -131,3 +147,12 @@ class JumboParticle extends NormalParticle
 	}
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
